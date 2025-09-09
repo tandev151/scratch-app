ddfcca95c445dd7cea7cfa0389d9b0256c0ab2f0
Le Duc Tan <tanld8@cyberspace.vn>
2025-09-09 12:15:30 +0700

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,52 @@
+import { fileURLToPath, URL } from 'url';
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+//ts-ignore;
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  console.log({ mode });
+  const commonConfig = {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        // Sử dụng import.meta.url để có đường dẫn an toàn và chính xác
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  };
+
+  // Cấu hình cho môi trường Development
+  if (command === 'serve' || mode === 'development') {
+    return {
+      ...commonConfig,
+      server: {
+        port: 3000,
+        // proxy: {
+        //   '/api': {
+        //     target: 'http://localhost:8080',
+        //     changeOrigin: true,
+        //   },
+        // },
+      },
+    };
+  }
+
+  // Cấu hình cho môi trường Production
+  // if (command === 'build' || mode === 'production') {
+  //   return {
+  //     ...commonConfig,
+  //     build: {
+  //       sourcemap: 'hidden', // Tạo source map nhưng không link trong file bundle
+  //       // rollupOptions: {
+  //       //   // Thêm plugin phân tích bundle chỉ khi build
+  //       //   plugins: [visualizer({ open: true })],
+  //       // },
+  //     },
+  //     // Có thể đặt base path cho production nếu bạn deploy vào một thư mục con
+  //     // base: '/my-app/',
+  //   };
+  // }
+
+  return commonConfig;
 });
